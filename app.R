a8766bcc8ea277960250b1570a6624c03d22eece
Alexander Dank <alexander.dank@outlook.com>
2023-08-06 00:02:40 +0200

    Initial commit
    Deployment of simple app

---
+++ app.R
@@ -0,0 +1,72 @@
+
+## load required libraries
+library(shiny)
+library(qpdf)
+library(shinythemes)
+
+
+# Define UI for application that combines PDFs
+ui <- fluidPage(theme = shinytheme("cosmo"),
+
+    # Application title
+    titlePanel("PDF merger app"),
+    
+    ## App layout to sidebar
+    sidebarLayout(
+    
+      ## sidebar for selecting the pdf files to merge
+    sidebarPanel(ID = "file_input",
+                 
+                 #file input code for input multiple pdfs
+                 fileInput('file', 'File input', multiple = TRUE, accept =c('.pdf')),
+                 
+                 #download button for downloading the merged pdfs
+                 downloadButton('download', "Download merged pdf files")
+    ),
+    
+    ## main panel for showing which files have been entered.
+    mainPanel(ID = 'file_table_out', DT::DTOutput('files')
+      
+    )
+  )
+)
+
+# Define server logic required to merge pdfs
+server <- function(input, output) {
+
+  ## show which files are uploaded by the user
+  output$files <- DT::renderDT({
+    ## code to show an error message when no pdf files are uploaded
+    validate(
+      need(!is.na(input$file), "Please upload pdf files to merge")
+    ) 
+    
+    ## code to produce a table of uploaded files
+    DT::datatable(input$file, selection = c("single"),
+                  options = list(
+                    scrollX = TRUE
+                  ))
+  })
+  
+  ## reactive extracting the paths of the uploaded files needed for qpdf
+  rval_pdf_inputs <- reactive({
+    datapaths <- c()
+      for (i in input$file$datapath) {
+        data <- subset(input$file, datapath == i)
+        datapaths <- append(datapaths, data$datapath)
+      }
+    datapaths
+  })
+  
+  ## download button for merged pdfs
+   output$download <- downloadHandler(
+     filename = "merged.pdf",
+     content = function(file) {
+       # function for combining the pdfs into one file
+       qpdf::pdf_combine(input = rval_pdf_inputs(), file)
+     })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
